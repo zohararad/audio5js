7f00e4d5edf4f92c57ad85f199efd6f8b7a39334
Zohar Arad <zohar@zohararad.com>
2013-01-12 11:23:12 +0200

    add global Audio5js package to ender

--- src/ender.js
+++ src/ender.js
@@ -1,3 +1,9 @@
-ender.ender({
-  audio5js: require('audio5js')
-});
+(function ($) {
+  "use strict";
+
+  var Audio5js = require('audio5js');
+  $.ender({
+    Audio5js: Audio5js
+  });
+  provide("Audio5js", Audio5js);
+}(ender));
