6c0c15389f7dc44401d53076a7f41f341e68c5b1
Zohar Arad <zohar@zohararad.com>
2013-01-30 21:02:52 +0200

    stub commit

--- src/audio5.js
+++ src/audio5.js
@@ -148,24 +148,24 @@
       var a = document.createElement('audio');
       var mime_str;
       switch (mime_type) {
-        case 'mp3':
-          mime_str = 'audio/mpeg; codecs="mp3"';
-          break;
-        case 'vorbis':
-          mime_str = 'audio/ogg; codecs="vorbis"';
-          break;
-        case 'opus':
-          mime_str = 'audio/ogg; codecs="opus"';
-          break;
-        case 'webm':
-          mime_str = 'audio/webm; codecs="vorbis"';
-          break;
-        case 'mp4':
-          mime_str = 'audio/mp4; codecs="mp4a.40.5"';
-          break;
-        case 'wav':
-          mime_str = 'audio/wav; codecs="1"';
-          break;
+      case 'mp3':
+        mime_str = 'audio/mpeg; codecs="mp3"';
+        break;
+      case 'vorbis':
+        mime_str = 'audio/ogg; codecs="vorbis"';
+        break;
+      case 'opus':
+        mime_str = 'audio/ogg; codecs="opus"';
+        break;
+      case 'webm':
+        mime_str = 'audio/webm; codecs="vorbis"';
+        break;
+      case 'mp4':
+        mime_str = 'audio/mp4; codecs="mp4a.40.5"';
+        break;
+      case 'wav':
+        mime_str = 'audio/wav; codecs="1"';
+        break;
       }
       if (mime_str === undefined) {
         throw new Error('Unspecified Audio Mime Type');
