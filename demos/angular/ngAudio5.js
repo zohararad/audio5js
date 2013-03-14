7afeb80360466c511022b16c33d32b51b81a1cb0
Zohar Arad <zohar@zohararad.com>
2013-03-14 08:43:43 +0200

    update angular demo to work with async load support

--- demos/angular/ngAudio5.js
+++ demos/angular/ngAudio5.js
@@ -1,14 +1,13 @@
-angular.module('Audio5', []).
-	factory('AudioService', function () {
-		var params = {
-			swf_path:'../../swf/audio5js.swf',
-			throw_errors:true,
-			format_time:true,
-			ready:function () {
+angular.module('Audio5', []).factory('AudioService', function () {
+  "use strict";
 
-			}
-		}
-		var audio5js = new Audio5js(params);
+  var params = {
+    swf_path:'../../swf/audio5js.swf',
+    throw_errors:true,
+    format_time:true
+  };
 
-		return audio5js;
-	})
+  var audio5js = new Audio5js(params);
+
+  return audio5js;
+});
